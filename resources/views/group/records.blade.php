07c4d2ae250581eb854744646359f269e548dd50
shougo3358 <yamawakishougo00@gmail.com>
2026-04-08 21:29:26 +0900

    f

--- resources/views/group/records.blade.php
+++ resources/views/group/records.blade.php
@@ -4,7 +4,7 @@
 @section('content')
 
 <div class="container py-3">
-
+<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
 
 <h4>{{ $group->name }}（正規連）</h4>
