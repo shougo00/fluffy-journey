787586d152503678f592719a3c9dc022ea777f27
shougo3358 <yamawakishougo00@gmail.com>
2026-05-03 20:06:32 +0900

    t

--- vite.config.js
+++ vite.config.js
@@ -9,7 +9,12 @@ export default defineConfig({
                 'resources/js/app.js',
                 'resources/css/camera/camera.css', 
                 'resources/css/group_history/index.blade.css',
-                'resources/css/group_history/monthly.css'
+                'resources/css/group_history/monthly.css',
+                'resources/js/camera/camera.js',
+                'resources/js/group/records.js',
+                'resources/css/kyudo_results/index.css',
+                'resources/css/lineup/index.css',
+                'resources/css/group/records.css'
             ],
             refresh: true,
         }),
