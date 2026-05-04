afa2b42d7546d9f5f88eeb5297c32170b63a583a
shougo3358 <yamawakishougo00@gmail.com>
2026-05-04 13:56:40 +0900

    e

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>新規登録</title>
-
+    <meta name="apple-mobile-web-app-capable" content="yes">
     <!-- Bootstrap 5 -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
