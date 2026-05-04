f984c0b5c8c65f532444fec45148db56109f07c4
shougo3358 <yamawakishougo00@gmail.com>
2026-05-04 12:28:48 +0900

    x

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -23,6 +23,7 @@ window.historyPageData = {
 <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
 @vite(['resources/css/dashboard/dashboard.css', 'resources/js/dashboard/dashboard.js'])
 <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
 
 
 <div class="container py-2">
